4f80d9d8da9c1aab4c78891a49e418442d5a9214
kira607 <kirill.lesckin@gmail.com>
2021-01-30 20:51:47 +0000

    update method added

--- flask_root.py
+++ flask_root.py
@@ -1,6 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import git
 
 app = Flask(__name__)
+
+@app.route('/pull_updates', methods=['POST'])
+def pull_updates():
+    if request.method == 'POST':
+        repo = git.Repo('.')
+        origin = repo.remotes.origin
+        origin.pull()
+        return 'Update server: success', 200
+    else:
+        return 'Wrong request type', 400
 
 @app.route('/')
 def main():
