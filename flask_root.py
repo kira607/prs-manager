0d98d819b94508a0c4eb4b52bc827182e6054e05
kira607 <kirill.lesckin@gmail.com>
2021-01-31 18:01:30 +0300

    move webhook handler in Service

--- flask_root.py
+++ flask_root.py
@@ -1,14 +1,9 @@
 from flask import Flask, request, make_response
-from update_validator import is_valid_signature
 from service import Service
-import git
-import os
 
 app = Flask(__name__)
 
 service = Service()
-
-SECRET_TOKEN = os.getenv('SECRET_TOKEN')
 
 
 @app.route('/table', methods=['GET'])
@@ -20,20 +15,9 @@ def get_table():
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    x_hub_signature = request.headers.get('X-Hub-Signature')
-    if SECRET_TOKEN is None:
-        print("Secret is None. Deploy failed...")
-        return "Update server: failed (secret token is not configured)", 500
-    else:
-        if not is_valid_signature(x_hub_signature, request.data, SECRET_TOKEN):
-            print("Deploy failed")
-    if request.method == 'POST':
-        repo = git.Repo('.')
-        origin = repo.remotes.origin
-        origin.pull()
-        return 'Update server: success', 200
-    else:
-        return 'Wrong request type', 400
+    resp, code = service.webhook(request)
+    response = make_response(resp, code)
+    return response
 
 @app.route('/')
 def main():
--- service.py
+++ service.py
@@ -1,13 +1,18 @@
 from db_client import DbClient
 from mail_client import MailClient
-from typing import Tuple
+from Flask import request as flask_request
+from update_validator import is_valid_signature
+import git
+import os
+from typing import Tuple, Union
 
 class Service:
     def __init__(self):
+        self.SECRET_TOKEN = os.getenv('SECRET_TOKEN')
         self.db_client = DbClient()
         self.mail_client = MailClient()
 
-    def get_table(self) -> Tuple[dict, int]:
+    def get_table(self) -> Tuple[Union[dict, str], int]:
         code = 200
         try:
             response = self.db_client.get_table()
@@ -17,8 +22,26 @@ class Service:
             code = 500
         return response, code
 
-    def webhook(self) -> Tuple[dict, int]:
-        pass
+    def webhook(self, request: flask_request) -> Tuple[Union[dict, str], int]:
+        code = 200
+        x_hub_signature = request.headers.get('X-Hub-Signature')
+        if self.SECRET_TOKEN is None:
+            code = 500
+            msg = "Update server: failed (secret token is not configured)"
+            response = self.__make_error_response(msg, code)
+        elif not is_valid_signature(x_hub_signature, request.data, self.SECRET_TOKEN):
+            code = 404
+            msg = "Invalid token, deploy aborted."
+            response = self.__make_error_response(msg, code)
+        elif request.method == 'POST':
+            repo = git.Repo('.')
+            origin = repo.remotes.origin
+            origin.pull()
+            response 'Update server: success'
+        else:
+            code = 400
+            response = 'Wrong request type'
+        return response, code
 
     def __make_error_response(self, msg: str, code: int) -> dict:
         return {
