79a35b67a325aa1f7d86a1fdc0fd12f970bcc5fb
kira607 <kirill.lesckin@gmail.com>
2021-02-15 11:26:27 +0000

    some changes in the db client

--- db_client.py
+++ db_client.py
@@ -1,10 +1,14 @@
-from MySQLdb import _mysql as mysql 
+from MySQLdb import _mysql as mysql
+import os
 
 
 class DbClient:
     def __init__(self):
-        # client = mysql.connect()
-        pass
+        host = os.getenv('DB_HOST')
+        user = os.getenv('DB_USER')
+        passwd = os.getenv('DB_PASSWORD')
+        db = os.getenv('DB_NAME')
+        self.client = mysql.connect(host=host, user=user, passwd=passwd, db=db)
 
     def get_table(self) -> dict:
         return {"table status": "empty"}
@@ -12,3 +16,6 @@ class DbClient:
     def update(self, data: dict) -> None:
         pass
 
+if __name__ == '__main__':
+    client = DbClient()
+    print(client.get_table())
