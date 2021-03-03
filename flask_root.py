c59b18bf7a6d65e4471a14a7dd2d278070e0f948
kira607 <kirill.lesckin@gmail.com>
2021-03-03 21:02:51 +0300

    flask admin instance creation test

--- flask_root.py
+++ flask_root.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, make_response
-# from flask_admin import Admin
+from flask_admin import Admin
 from service import Service
 
 app = Flask(__name__)
+app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
 
-# app.config['FLASK_ADMIN_SWATCH'] = 'cerulean'
-# admin = Admin(app, name='prs-manager')
+admin = Admin(app, name='prs-manager')
 
 service = Service()
 
